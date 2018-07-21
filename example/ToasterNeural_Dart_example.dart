0d5ec8d3a4e73733c21e73d3dfe6ca02376d9f50
EDToaster <edt.chen@mail.utoronto.ca>
2018-07-20 17:07:17 -0700

    Initial Commit

---
+++ example/ToasterNeural_Dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:ToasterNeural_Dart/ToasterNeural_Dart.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/ToasterNeural_Dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library ToasterNeural_Dart;
+
+export 'src/ToasterNeural_Dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
